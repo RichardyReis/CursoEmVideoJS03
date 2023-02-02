b2515843cb9fba4e033a739648519d2858289365
RichardyReis <richardyleonardoreis@gmail.com>
2023-02-02 18:11:37 -0300

    3º projeto do curso de JavaScript do Curso em Vídeo

---
+++ script.js
@@ -0,0 +1,22 @@
+//var n1 = window.prompt('Digite um número: ');       string
+//var n2 = window.prompt('Digite outro número: ');    string
+//var s = n1 + n2;                                    Concatenação
+//window.alert('A soma dos valores é ' + s);
+//(número + número)                                   Adição
+//(string + string)                                   Concatenação
+
+//var n1 = Number.parseInt(window.prompt('Digite um número: '));       Conversão de string para Int
+//var n2 = Number.parseInt(window.prompt('Digite outro número: '));    Conversão de string para Int
+//var n1 = Number.parseFloat(window.prompt('Digite um número: '));     Conversão de string para Float
+//var n2 = Number.parseFloat(window.prompt('Digite outro número: '));  Conversão de string para Float
+var n1 = Number(window.prompt('Digite um número: '));                  //Conversão de automática
+var n2 = Number(window.prompt('Digite outro número: '));               //Conversão de automática
+var s = n1 + n2;                                                       //Número + número = soma
+window.alert(`A soma entre ${n1} e ${n2} é ${s}`);
+//String(variável)
+//variável.toString()
+//variável.length                                                      determina quantos caracteres a string tem
+//variável.toUpperCase()                                               coloca a variável em maiúsculo
+//variável.toLowerCase()                                               coloca a variável em minúsculo                  
+
+
